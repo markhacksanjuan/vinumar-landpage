052ffe4ffb89628206a8d129680c557c4cd10eb5
Mark <mark.sanjuan.sole@gmail.com>
2021-10-29 18:37:37 +0200

    added images and carousels

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,24 +1,33 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1
+let slideIndex2 = 1
+
+const showSlides = (n) => {
+  const slides = document.getElementsByClassName("mySlides")
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"
+  }
+  
+  slides[slideIndex-1].style.display = "block"
+}
+const showSlides2 = (n) => {
+  const slides = document.getElementsByClassName('mySlides2')
+  if(n > slides.length) {slideIndex2 = 1}
+  if(n < 1) {slideIndex2 = slides.length}
+  for(i = 0; i < slides.length; i++){
+    slides[i].style.display = 'none'
+  }
+  slides[slideIndex2 - 1].style.display = 'block'
+}
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+const plusSlides2 = (n) => {
+  showSlides2(slideIndex2 += n)
 }
 
-function showSlides(n) {
-  let i
-  let slides = document.getElementsByClassName("mySlides")
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"
-  }
-
-  slides[slideIndex-1].style.display = "block"
-}
+showSlides2(slideIndex2)
+showSlides(slideIndex)
